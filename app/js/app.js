236cad1f0235db16b580a5bfc1012038a7f86fe6
Andrew Vasilchenko <cazonove@gmail.com>
2023-09-24 14:34:10 +0300

    added

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,29 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	
+	// Получаем все обертки видео
+	const videoWrappers = document.querySelectorAll('.block-video-wrapper');
+
+	// Перебираем каждую обертку видео
+	videoWrappers.forEach(wrapper => {
+		// Получаем видео и span элементы внутри текущей обертки
+		const video = wrapper.querySelector('video');
+		const playIcon = wrapper.querySelector('.icon-video-play');
+
+		// Добавляем обработчик события 'play' для видео
+		video.addEventListener('play', () => {
+			// Скрываем span с классом 'icon-video-play'
+			playIcon.classList.add('hide');
+			video.setAttribute('controls', '');
+		});
+
+		// Добавляем обработчик события 'pause' для видео
+		video.addEventListener('pause', () => {
+			// Отображаем span с классом 'icon-video-play'
+			playIcon.classList.remove('hide');
+			video.removeAttribute('controls');
+		});
+	});
+
 
 })
