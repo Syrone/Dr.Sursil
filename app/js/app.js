f09e75eb94929efe469b13d074afcc1c55ec8402
Andrew Vasilchenko <cazonove@gmail.com>
2023-09-25 04:00:34 +0300

    ready

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,3 @@
-// // Import vendor jQuery plugin example
-// import '~/app/js/jquery-3.7.1.min.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -35,6 +33,56 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 	/** (End) Динамическая ширина Input Quantity **/
+
+
+	/** (Start) Quiz Slick **/
+	$('.quiz-slick').slick({
+		swipe: false,
+		fade: true,
+		dots: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		appendArrows: '.quiz-slick-buttons',
+		prevArrow: '<button class="btn btn-outline btn-quiz slick-prev" type="button">Назад</button>',
+		nextArrow: '<button class="btn btn-primary btn-quiz slick-next" type="button">Далее</button>',
+	});
+
+	$(document).ready(function () {
+		var totalSlides = $('.quiz-slick').slick('getSlick').slideCount - 1;
+
+		for (var i = 0; i < totalSlides; i++) {
+			var listItem = $('<li>').addClass(i === 0 ? 'active' : '');
+			var currentStep = $('<span>').addClass('quiz-header-current').text('Шаг');
+			var totalSteps = $('<span>').addClass('quiz-header-total').text('из ').append($('<span>'));
+
+			listItem.append(currentStep, totalSteps);
+			$('.quiz-header').append(listItem);
+		}
+
+		$('.quiz-header-total span').text(totalSlides);
+	});
+
+	$('.quiz-slick').on('init', function (event, slick) {
+		const activeSlideIndex = slick.currentSlide;
+		$('.quiz-header li').eq(activeSlideIndex).addClass('active');
+	});
+
+	$('.quiz-slick').on('afterChange', function (event, slick, currentSlide) {
+		$('.quiz-header li').removeClass('active accent');
+		$('.quiz-header li').eq(currentSlide).addClass('active');
+		$('.quiz-header li').eq(currentSlide).prevAll().addClass('accent');
+
+		if ($('.quiz-slick .quiz-slide-thx').hasClass('slick-current')) {
+			$('.btn-quiz-thx').removeClass('hide');
+			$('.quiz-header').addClass('hide');
+		} else {
+			$('.btn-quiz-thx').addClass('hide');
+			$('.quiz-header').removeClass('hide');
+		}
+	});
+	/** (End) Quiz Slick **/
 
 
 })
